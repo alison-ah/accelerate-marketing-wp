46cf1cb5b73efdd9d14ce2a91eefc8ab2462f119
Alison <alison@andersonholland.com>
2025-02-06 09:13:20 -0600

    Styled featured work on homepage

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -22,14 +22,38 @@ get_header(); ?>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
+
+	<section class="featured-work">
+	<div class="site-content clearfix">
+		<h2>Featured Work</h2>
+		<ul class="homepage-featured-work">
+		<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+		<!-- the loop -->
+			<?php while ( have_posts() ) : the_post();
+				$image_1 = get_field("image_1");
+				$size = "medium";
+			?>
+			<li class="individual-featured-work">
+				<a href="<?php the_permalink(); ?>">
+					<figure>
+						<?php echo wp_get_attachment_image($image_1, $size); ?>
+					</figure>
+					<h3><?php the_title(); ?></h3>
+				</a>
+			</li>
+			<?php endwhile; // end of the loop. ?>
+			<?php wp_reset_query(); // resets the altered query back to the original ?>
+		</ul>
+	</div>
+	</section>
 	<section class="recent-posts">
  		<div class="site-content">
 			<div class="blog-post">
-				<h4>From the Blog</h4>
+				<h2>From the Blog</h2>
 				<?php query_posts('posts_per_page=1'); ?>
 					<?php while ( have_posts() ) : the_post(); ?>
 						<h3><?php the_title(); ?></h3>
-						<?php the_excerpt(); ?> 
+						<p><?php the_excerpt(); ?></p>
 					<?php endwhile; ?> 
 				<?php wp_reset_query(); ?>
 			</div>
