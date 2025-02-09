e3867dbe59b1a713968c8c86efc8db72d08dbea4
Alison <alison@andersonholland.com>
2025-02-08 19:14:36 -0600

    Completed 404 page

--- wp-content/themes/accelerate-theme-child/404.php
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -3,8 +3,8 @@
  * The template for displaying 404 pages (Not Found)
  *
  * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
  */
 
 get_header(); ?>
@@ -13,16 +13,39 @@ get_header(); ?>
 		<div id="content" class="site-content" role="main">
 
 			<header class="page-header">
-				<h1 class="page-title"><?php _e( 'Not Found', 'twentythirteen' ); ?></h1>
+				<h1 class="page-title"><?php _e( 'Not Found', 'accelerate-marketing' ); ?></h1>
 			</header>
 
 			<div class="page-wrapper">
-				<div class="page-content">
-					<h2><?php _e( "Ope! It looks like you're a little lost.", 'twentythirteen' ); ?></h2>
-					<p><?php _e( "Maybe I can point you in the right direction. Have you tried looking at our <a href="<?php echo site_url('/blog'); ?>">blog</a> or at our <a href="<?php echo site_url('/about'); ?>"> services </a>? Or maybe you're looking for our<a href="<?php echo site_url('/case-studies'); ?>"> featured work</a>. You can always<a href="<?php echo site_url('/contact'); ?>"> reach out</a> if you have a question.", 'twentythirteen' ); ?></p>
-					<figure><img src="https://images.unsplash.com/photo-1464716821973-e1031cfa43dc?q=80&w=2145&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" /></figure>
+				<div class="page-content" id="not-found-page">
+					<div class="text">
+						<h2 class="not-found"><?php _e( "Ope!", 'accelerate-marketing' ); ?></h2>
+							<h3 class="not-found"><?php _e( "It looks like you're a little lost.", 'accelerate-marketing' ); ?></h3>
+
+								<p class="not-found highlight-links">
+									<?php
+										echo sprintf(
+											"Maybe I can point you in the right direction. Have you tried looking at our 
+											<a href=\"%s\">blog</a> 
+											or at our <a href=\"%s\">services</a>
+											? Or maybe you're looking for our 
+											<a href=\"%s\">featured work</a>
+											. You can always 
+											<a href=\"%s\">reach out</a> 
+											if you have a question.",
+											site_url('/blog/'),
+											site_url('/about/'),
+											site_url('/case-studies/'),
+											site_url('/contact/')
+										);
+									?>
+								</p>
+
+					</div><!-- .text -->
+					<figure id="not-found-img">
+						<img src="https://images.unsplash.com/photo-1464716821973-e1031cfa43dc?q=80&w=2145&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" style="max-width: 70%; height: auto;" />
+					</figure>
 					
-					<?php get_search_form(); ?>
 				</div><!-- .page-content -->
 			</div><!-- .page-wrapper -->
 
